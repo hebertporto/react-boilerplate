3cfc96b54e5917215ab21cafa54dd1c3e8d739fa
Hebert Porto <hebertporto@gmail.com>
2017-09-08 20:57:36 -0300

    Adicionando fluxo do redux
    incluindo arquivos para gerar o fluxo do redux

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './style.less'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './store'
+import App from './containers/app'
 
-ReactDOM.render(
-  <h1>Olá React</h1>
-, document.getElementById('root'))
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root'))
